ff304b64e398b7e22a63e70a0281703a0792ff9e
Burhan Yusuf <burhanyusuf@outlook.com>
2020-12-17 16:30:13 +0700

    add WishList

--- js/fetch.js
+++ js/fetch.js
@@ -2,6 +2,8 @@ const apiKey = 'c3fd6f0f39760614747cf702d6c44b11';
 const apiUrl = 'https://api.themoviedb.org';
 const imgPath = 'https://image.tmdb.org/t/p/original/';
 const urlParams = new URLSearchParams(window.location.search);
+
+//mendapatkan data genre
 if(!localStorage.genre){
     fetch(`${apiUrl}/3/genre/movie/list?api_key=${apiKey}&language=en-US`).then(function(response){
     if (response.status !== 200) {
@@ -16,25 +18,31 @@ if(!localStorage.genre){
 })
 }
 
+//mendapatkan halaman sekarang
 let page = urlParams.get('page') || 1;
 
+//menampilkan data film
 function renderMovie(movie, index,movieList){
     var movieContainer = document.getElementById('movie-container');
     if(index % 5 == 0){
         let newRow = `<div class="row"></div>`;
         movieContainer.insertAdjacentHTML('beforeend', newRow);
     }
-    let movieCard = `<div class="movieCard">`;
+    let movieCard = `<div class="movieCard"><a href="movie.html?movie=${movie.id}">`;
+
     let yearRelease = movie.release_date.split('-')[0];
     movieCard+= `<img src="${imgPath}${movie.poster_path}" style="width:100%;"/>`
     movieCard+= `<span class="titleMovie">${movie.title}<br /> ( ${yearRelease} )</span><br>`;
+
     movieCard+= `<div class="genre">`;
     for(let i=0;i<movie.genre_ids.length;i++){
         let genre_id = movie.genre_ids[i];
         let genre = JSON.parse(localStorage.getItem("genre")).genres.find(o => o.id === genre_id);
         movieCard+= `<div class="genreMovie">${genre.name}</div>`
     }
+    movieCard+= `</a><button type="button" onClick='addToWishList(${JSON.stringify(movie)})' class="addToWishList">WishList</button>`
     movieCard+= `</div>`;
+
     movieContainer.insertAdjacentHTML('beforeend', movieCard);
     if(index == movieList.length - 1){
         let pagination = `<div class="pagination">`;
@@ -43,11 +51,12 @@ function renderMovie(movie, index,movieList){
         for(let i=firstPage; i<=lastPage;i++){
             pagination+=`<a href="?page=${i}">${i}</a>`
         }
-        pagination+=`</div>`;
+        pagination+=`</a></div>`;
         movieContainer.insertAdjacentHTML('beforeend', pagination);
     }
 }
 
+//mendapatkan data film
 (function () {
     fetch(`${apiUrl}/3/movie/popular?api_key=${apiKey}&language=en-US&page=${page}`)
     .then(
@@ -68,3 +77,18 @@ function renderMovie(movie, index,movieList){
       });
     ;
 })();
+
+
+//menambah ke wishlist
+function addToWishList(movie){
+    let wishList = {"movies" : []};
+
+    if(localStorage.wishList){
+        wishList.movies = JSON.parse(localStorage.wishList);
+    }
+
+    wishList.movies.push(movie);
+
+    localStorage.setItem('wishList',wishList);
+
+}
